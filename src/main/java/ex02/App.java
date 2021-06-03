8250ef6ba0003bd0dab5d474de993ccea0105286
savosburn <savosburn18@tampabay.rr.com>
2021-06-03 15:53:33 -0400

    Initial commit

---
+++ src/main/java/ex02/App.java
@@ -0,0 +1,29 @@
+package ex02;
+
+import java.util.Scanner;
+
+/*
+ *  UCF COP3330 Summer 2021 Assignment 1 Solution
+ *  Copyright 2021 Savannah Osburn
+ */
+
+
+/*
+Create a program that prompts for an input string and displays output that shows the input string and the number of characters the string contains.
+
+Example Output
+What is the input string? Homer
+Homer has 5 characters.
+
+Constraints
+Be sure the output contains the original string.
+Use a single output statement to construct the output.
+Use a built-in function of the programming language to determine the length of the string.
+ */
+public class App {
+    static Scanner in = new Scanner(System.in);
+
+    public static void main(String[] args) {
+    }
+
+}
